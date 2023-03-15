859e99bfaac7f5c4ac985d2fa5157bb8018efe92
aca19aio <aiokan1@sheffield.ac.uk>
2023-03-15 22:42:53 +0000

    Modified repo to have it as a module

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+setup(
+  name = 'analysing-bee-behaviour',
+  packages = ['analysing-bee-behaviour'],
+  version = '1.0',
+  description = 'Analysing bumblee behaviour from videos',
+  author = 'Aslihan Ilgin Okan',
+  author_email = 'aslihanilgin@outlook.com',
+  url = 'https://github.com/aslihanilgin/analysing-bee-behaviour.git',
+  download_url = 'https://github.com/aslihanilgin/analysing-bee-behaviour.git',
+  keywords = ['computer vision','ML','object detection','sample'],
+  classifiers = [],
+  install_requires=['roboflow','numpy', 'cv2'],
+)
